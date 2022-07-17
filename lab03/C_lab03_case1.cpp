7225e1d52ad8c4e12b8f0d7e997e077bef820078
Teerawat <82719459+Teerawa-ctrl@users.noreply.github.com>
2022-07-17 19:28:43 +0700

    Add files via upload

---
+++ lab03/C_lab03_case1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main (){
+		int five = 5 ;
+		float six = 12.24 ; 
+		char txt = 'f';
+		printf ( "%d\n %f\n %c\n ",five,six,txt ) ; 
+		return 0 ;
+}
---
+++ lab03/C_lab03_case2.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main (){
+		int a = 9 ;
+		float b = 13.1 ;
+		char c = 'M' ;
+		printf( " %d\n %f\n %c\n",a,b,c ) ;
+		return 0 ;
+}
