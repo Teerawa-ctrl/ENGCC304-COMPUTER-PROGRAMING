a3b7b12b4b43e5c238e0c5cd9c5f19922299af90
Teerawat <82719459+Teerawa-ctrl@users.noreply.github.com>
2022-07-18 22:09:32 +0700

    Add files via upload

---
+++ C_lab04_01.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main (){
+	char Eid[] = "" ; //variable of employeesid
+	int hr     = 0 ;  //variable of hour
+	int sla    = 0 ;  //variable of salary
+	
+	while(true){
+		printf("Input your Employees_ID (*Warning maximum 10 chars*) : ");
+        scanf("%s",Eid);
+        if(strlen(Eid) <= 10) break;
+	}//end while
+		
+	
+	//
+	printf("input the working(hr)");
+	scanf("%d",&hr);
+	printf("Salary ammout/hr(bath)");
+	scanf("%d",&sla);
+	
+	
+	//show expected output
+	printf("------------------------------ \n");
+	printf("Expected Output \n" );
+	printf("Employees_ID = %s\n",Eid);
+	printf("Amount/day = %d bath(s)\n",(sla*hr));
+	printf("------------------------------ \n");
+
+	return 0 ;
+}//endfunction
+
