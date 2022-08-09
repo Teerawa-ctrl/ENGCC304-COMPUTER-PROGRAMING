402161c0baf07e8c6ce4cdfe6be08d5cde4fbd9b
Teerawat <82719459+Teerawa-ctrl@users.noreply.github.com>
2022-08-09 19:16:56 +0700

    Add files via upload

---
+++ lab6/lab6_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	printf("Input your line : ");
+	scanf("%d", &a);
+	
+	for(int y = 1; y<=a ; y++){
+    	for(int x=1 ; x<=y ; x++){
+    	  printf("*");
+    	}
+		printf("\n");
+	}
+	
+	return 0;
+}
---
+++ lab6/lab6_3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	printf("Input your line : ");
+	scanf("%d", &a);
+	
+	if(a%2==0){
+        for(int y = 1; y<=a ; y++){
+    	    for(int x=a-1 ; x>=y ; x--){
+                printf(" ");
+    	    }
+            for(int x=1; x<=y ; x++){
+                printf("* ");
+            }
+		printf("\n");
+	    }
+    }
+    else{
+        for(int y = 1; y<=a ; y++){
+    	    for(int x=2 ; x<=y ; x++){
+                printf(" ");
+    	    }
+            for(int x=a; x>=y ; x--){
+                printf("* ");
+            }
+		printf("\n");
+        }
+    }
+	
+	return 0;
+}
